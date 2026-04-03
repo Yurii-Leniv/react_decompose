aac65b2bb1dd9c26f4ade414b20ef7cfc301d4f3
mykhalenych <mykhalenych@gmail.com>
2026-04-03 13:56:12 +0300

    bugfix/h1 changed unnecessary h1 to h2

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => (
       </nav>
     </header>
     <article className="article">
-      <h1 className="article__title">Headline</h1>
+      <h2 className="article__title">Headline</h2>
       <p className="article__paragraph">
         In elementum lorem eget est euismod ornare. Phasellus sit amet
         pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliquam nibh.
